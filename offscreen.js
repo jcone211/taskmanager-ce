25e15d486ed35a83a140a595893b6d822fb92192
Oriol Llovera <oriollloveraroura@gmail.com>
2025-02-03 11:49:45 +0100

    Correccion error offscreen

--- offscreen.js
+++ offscreen.js
@@ -2,14 +2,8 @@ chrome.runtime.onMessage.addListener((message) => {
     if (message.type === 'playSound') {
         const audio = new Audio(chrome.runtime.getURL('alert.mp3'));
         audio.play()
-            .then(() => {
-                setTimeout(() => {
-                    chrome.offscreen.closeDocument();
-                }, 3000);
-            })
             .catch(error => {
                 console.error('Error al reproducir:', error);
-                chrome.offscreen.closeDocument();
             });
     }
 });
